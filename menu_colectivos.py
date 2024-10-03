d4984f6b2e60b11ce8a2d7a9b5aac02994b17677
joaquinleonardi <joaquin.n.leonardi@gmail.com>
2024-10-03 12:07:27 -0300

    subo tarea

---
+++ menu_colectivos.py
@@ -0,0 +1,23 @@
+from matrices_colectivo.colectivos import *
+
+print ("1- cargar planilla \n2- Mostrar la recaudación de cada coche y línea \n3- Calcular y mostrar recaudación por línea \n4- Calcular y mostrar recaudación por coche \n5- Calcular y mostrar la recaudación total \n6- salir")
+opcion = int(input("ingrese una opcion: "))
+
+while True:
+
+    match opcion:
+
+        case 1:
+            pass
+        case 2:
+            pass
+        case 3:
+            pass
+        case 4:
+            pass
+        case 5:
+            pass
+        case 6:
+            pass
+
+
